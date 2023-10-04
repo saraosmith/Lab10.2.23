005133998b251ae4003ceea658d3d54f0290638c
ssmith@domain.com <86253201+saraosmith@users.noreply.github.com>
2023-10-04 16:44:53 -0400

    word or two

--- src/lab/HelloWorld.java
+++ src/lab/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Second print statement alternate");
+		System.out.println("Second print statement alternate.");
+		System.out.println("a word or two");
 	}
 
 }
